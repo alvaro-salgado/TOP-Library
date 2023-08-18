c0f6d771a3170c378fbc9a8f17348c466af02bd4
AlvaroSalgado <alvaro.salgado.lopez@gmail.com>
2023-08-17 21:09:03 -0600

    Adding dialog and js

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
+const btnAddBook = document.querySelector('.btn-add-book');
+const bookDialog = document.getElementById('book-dialog');
+const bookData = document.querySelector('.book-form');
+const submitButton = document.querySelector('.book-submit-btn')
+
+console.log(bookData);
+
+
 const myLibrary = [];
+
+
+//Show the dialog
+
+btnAddBook.addEventListener('click', () =>{
+    bookDialog.showModal();
+})
 
 
 
@@ -22,12 +37,12 @@ function addBookToLibrary (title, author, pagesNo, isRead){
     myLibrary.push(book);
 }
 
-const book1 = new Book('Hobbit','Me',164, false);
+// const book1 = new Book('Hobbit','Me',164, false);
 
-console.log(book1.info());
+// console.log(book1.info());
 
-console.log(myLibrary)
-addBookToLibrary('Harry Potter', 'JK Rowling','234',false)
-addBookToLibrary('Harry', 'JK Rowling','234',false)
+// console.log(myLibrary)
+// addBookToLibrary('Harry Potter', 'JK Rowling','234',false)
+// addBookToLibrary('Harry', 'JK Rowling','234',false)
 
-console.log(myLibrary[1].title)
+// console.log(myLibrary[1].title)
